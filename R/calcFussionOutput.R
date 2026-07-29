40c128151ad2fa4bb4308c1228a0e75cc0cc448c
Fsioutas <fsioutas@hotmail.com>
2026-07-29 17:39:47 +0300

    refactor calcFussionOutput function for improved readability and maintainability; update variable mappings and join operations

--- R/calcFussionOutput.R
+++ R/calcFussionOutput.R
@@ -31,26 +31,65 @@ calcFussionOutput <- function() {
     rename_with(~ sub("^SETS\\.", "", .x))
 
   variable_mapping <- c(
-    AnnualEmissions = "Emissions",
-    AnnualFixedOperatingCost = "Cost|Energy Supply|Electricity|Fixed O&M",
-    AnnualTechnologyEmission = "Emissions|Energy|Supply|Electricity",
-    AnnualVariableOperatingCost = "Cost|Energy Supply|Electricity|Variable O&M",
-    CapitalInvestment = "Investment|Energy Supply|Electricity",
-    Demand = "Final Energy|Electricity",
-    DiscountedSalvageValue = "Model Accounting|Discounted Salvage Value",
-    NewCapacity = "Capacity Additions|Electricity",
-    ProductionByTechnology = "Secondary Energy|Electricity",
-    ProductionByTechnologyAnnual = "Secondary Energy|Electricity",
-    RateOfActivity = "Activity|Electricity",
-    SalvageValue = "Model Accounting|Salvage Value",
-    TotalAnnualTechnologyActivityByMode = "Activity|Electricity",
-    TotalCapacityAnnual = "Capacity|Electricity",
-    TotalTechnologyAnnualActivity = "Activity|Electricity",
-    UseByTechnologyAnnual = "Technology Input",
-    Capital_costs = "Capital Cost|Electricity",
-    Fixed_costs = "Fixed Cost|Electricity",
-    Variable_costs = "Variable Cost|Electricity",
-    Fuel_costs = "Fuel Cost|Electricity"
+    AnnualEmissions =
+      "Emissions",
+
+    AnnualFixedOperatingCost =
+      "Cost|Energy Supply|Electricity|Fixed O&M",
+
+    AnnualTechnologyEmission =
+      "Emissions|Energy|Supply|Electricity",
+
+    AnnualVariableOperatingCost =
+      "Cost|Energy Supply|Electricity|Variable O&M",
+
+    CapitalInvestment =
+      "Investment|Energy Supply|Electricity",
+
+    Demand =
+      "Final Energy|Electricity",
+
+    DiscountedSalvageValue =
+      "Model Accounting|Discounted Salvage Value",
+
+    NewCapacity =
+      "Capacity Additions|Electricity",
+
+    ProductionByTechnology =
+      "Secondary Energy|Electricity",
+
+    ProductionByTechnologyAnnual =
+      "Secondary Energy|Electricity",
+
+    RateOfActivity =
+      "Activity|Electricity",
+
+    SalvageValue =
+      "Model Accounting|Salvage Value",
+
+    TotalAnnualTechnologyActivityByMode =
+      "Activity|Electricity",
+
+    TotalCapacityAnnual =
+      "Capacity|Electricity",
+
+    TotalTechnologyAnnualActivity =
+      "Activity|Electricity",
+
+    UseByTechnologyAnnual =
+      "Technology Input",
+
+    Capital_costs =
+      "Capital Cost|Electricity",
+
+    Fixed_costs =
+      "Fixed Cost|Electricity",
+
+    Variable_costs =
+      "Variable Cost|Electricity",
+
+    Fuel_costs =
+      "Fuel Cost|Electricity"
   )
 
   technology_map <- SETS %>%
@@ -58,47 +97,87 @@ calcFussionOutput <- function() {
       t = Code,
       technology_iamc = Description
     ) %>%
-    filter(!is.na(t)) %>%
-    distinct(t, .keep_all = TRUE)
+    filter(
+      !is.na(t)
+    ) %>%
+    distinct(
+      t,
+      .keep_all = TRUE
+    )
 
   commodity_map <- SETS %>%
     transmute(
       f = Code.1,
       fuel_iamc = Description.1
     ) %>%
-    filter(!is.na(f)) %>%
-    distinct(f, .keep_all = TRUE)
+    filter(
+      !is.na(f)
+    ) %>%
+    distinct(
+      f,
+      .keep_all = TRUE
+    )
 
   DRC_results <- DRC_results %>%
     mutate(
-      e = recode(e, EMIC02 = "CO2"),
-      variable_iamc_base = unname(variable_mapping[variable])
+      e = recode(
+        e,
+        EMIC02 = "CO2"
+      ),
+
+      variable_iamc_base = unname(
+        variable_mapping[as.character(variable)]
+      )
     ) %>%
-    left_join(technology_map, by = "t") %>%
-    left_join(commodity_map, by = "f") %>%
+    left_join(
+      technology_map,
+      by = "t"
+    ) %>%
+    left_join(
+      commodity_map,
+      by = "f"
+    ) %>%
     mutate(
       variable_iamc = case_when(
         variable == "AnnualEmissions" ~
-          paste("Emissions", e, sep = "|"),
+          paste(
+            "Emissions",
+            e,
+            sep = "|"
+          ),
 
         variable == "AnnualTechnologyEmission" ~
           paste(
-            "Emissions", e, "Energy", "Supply", "Electricity",
+            "Emissions",
+            e,
+            "Energy",
+            "Supply",
+            "Electricity",
             technology_iamc,
             sep = "|"
           ),
 
-        !is.na(technology_iamc) ~
-          paste(variable_iamc_base, technology_iamc, sep = "|"),
-
         variable == "Demand" ~
-          paste(variable_iamc_base, fuel_iamc, sep = "|"),
+          paste(
+            variable_iamc_base,
+            fuel_iamc,
+            sep = "|"
+          ),
 
         variable == "ProductionByTechnology" ~
           paste(
             variable_iamc_base,
+            fuel_iamc,
             technology_iamc,
             paste0("Time Slice ", l),
+            sep = "|"
+          ),
+
+        variable == "ProductionByTechnologyAnnual" ~
+          paste(
+            variable_iamc_base,
+            fuel_iamc,
+            technology_iamc,
             sep = "|"
           ),
 
@@ -127,183 +206,283 @@ calcFussionOutput <- function() {
             sep = "|"
           ),
 
-        variable == "Fuel_costs" ~
+        !is.na(technology_iamc) ~
           paste(
             variable_iamc_base,
             technology_iamc,
+            sep = "|"
+          ),
+
+        !is.na(fuel_iamc) ~
+          paste(
+            variable_iamc_base,
             fuel_iamc,
             sep = "|"
           ),
 
-        TRUE ~ variable_iamc_base
+        TRUE ~
+          variable_iamc_base
       ),
+
       variable_iamc = variable_iamc %>%
-        gsub("\\|NA(?=\\||$)", "", ., perl = TRUE) %>%
-        gsub("\\|+", "|", .) %>%
-        sub("\\|$", "", .)
+        gsub(
+          "\\|NA(?=\\||$)",
+          "",
+          .,
+          perl = TRUE
+        ) %>%
+        gsub(
+          "\\|+",
+          "|",
+          .
+        ) %>%
+        sub(
+          "\\|$",
+          "",
+          .
+        )
     )
 
   DRC_results[["model"]] <- "RE-INTEGRATE"
 
-  ETH <- readSource("ETHIOPIA")  %>% as.quitte()
+  ETH <- readSource("ETHIOPIA") %>%
+    as.quitte()
 
   variable_mapping <- c(
-    TotalCapacityAnnual = "Capacity|Electricity",
-    AnnualizedInvestmentCost = "Cost|Energy Supply|Electricity|Annualized Investment",
-    AnnualTechnologyEmission = "Emissions|Energy|Supply|Electricity",
-    ProductionByTechnologyByMode = "Secondary Energy|Electricity"
+    TotalCapacityAnnual =
+      "Capacity|Electricity",
+    AnnualizedInvestmentCost =
+      "Cost|Energy Supply|Electricity|Annualized Investment",
+    AnnualTechnologyEmission =
+      "Emissions|Energy|Supply|Electricity",
+    ProductionByTechnologyByMode =
+      "Secondary Energy|Electricity"
   )
 
   ETH_results <- ETH %>%
     mutate(
-      e = recode(e, EMIC02 = "CO2"),
-      variable_iamc_base = unname(variable_mapping[variable])
+      e = recode(
+        e,
+        EMIC02 = "CO2"
+      ),
+      variable_iamc_base = unname(
+        variable_mapping[as.character(variable)]
+      )
     ) %>%
-    left_join(technology_map, by = "t") %>%
-    left_join(commodity_map, by = "f") %>%
+    left_join(
+      technology_map,
+      by = "t"
+    ) %>%
+    left_join(
+      commodity_map,
+      by = "f"
+    ) %>%
     mutate(
       variable_iamc = case_when(
-        variable == "AnnualEmissions" ~
-          paste("Emissions", e, sep = "|"),
-
         variable == "AnnualTechnologyEmission" ~
           paste(
-            "Emissions", e, "Energy", "Supply", "Electricity",
+            "Emissions",
+            e,
+            "Energy",
+            "Supply",
+            "Electricity",
             technology_iamc,
             sep = "|"
           ),
 
-        !is.na(technology_iamc) ~
-          paste(variable_iamc_base, technology_iamc, sep = "|"),
-
-        variable == "Demand" ~
-          paste(variable_iamc_base, fuel_iamc, sep = "|"),
-
-        variable == "ProductionByTechnology" ~
+        variable == "ProductionByTechnologyByMode" ~
           paste(
             variable_iamc_base,
-            technology_iamc,
-            paste0("Time Slice ", l),
-            sep = "|"
-          ),
-
-        variable == "RateOfActivity" ~
-          paste(
-            variable_iamc_base,
+            fuel_iamc,
             technology_iamc,
             paste0("Mode ", m),
             paste0("Time Slice ", l),
             sep = "|"
           ),
 
-        variable == "TotalAnnualTechnologyActivityByMode" ~
+        variable %in% c(
+          "TotalCapacityAnnual",
+          "AnnualizedInvestmentCost"
+        ) ~
           paste(
             variable_iamc_base,
-            technology_iamc,
-            paste0("Mode ", m),
-            sep = "|"
-          ),
-
-        variable == "UseByTechnologyAnnual" ~
-          paste(
-            variable_iamc_base,
-            fuel_iamc,
             technology_iamc,
             sep = "|"
           ),
 
-        variable == "Fuel_costs" ~
-          paste(
-            variable_iamc_base,
-            technology_iamc,
-            fuel_iamc,
-            sep = "|"
-          ),
-
-        TRUE ~ variable_iamc_base
+        TRUE ~
+          variable_iamc_base
       ),
+
       variable_iamc = variable_iamc %>%
-        gsub("\\|NA(?=\\||$)", "", ., perl = TRUE) %>%
-        gsub("\\|+", "|", .) %>%
-        sub("\\|$", "", .)
+        gsub(
+          "\\|NA(?=\\||$)",
+          "",
+          .,
+          perl = TRUE
+        ) %>%
+        gsub(
+          "\\|+",
+          "|",
+          .
+        ) %>%
+        sub(
+          "\\|$",
+          "",
+          .
+        )
     )
 
   MUS <- readSource("MAURITIUS")
 
   variable_mapping <- c(
-    AccumulatedNewCapacity = "Accumulated Capacity Additions|Electricity",
-    AnnualFixedOperatingCost = "Cost|Energy Supply|Electricity|Fixed O&M",
-    AnnualizedInvestmentCost = "Cost|Energy Supply|Electricity|Annualized Investment",
-    AnnualTechnologyEmission = "Emissions|Energy|Supply|Electricity",
-    AnnualTechnologyEmissionByMode = "Emissions|Energy|Supply|Electricity",
-    AnnualVariableOperatingCost = "Cost|Energy Supply|Electricity|Variable O&M",
-    CapitalInvestment = "Investment|Energy Supply|Electricity",
-    Demand = "Final Energy|Electricity",
-    DiscountedSalvageValue = "Model Accounting|Discounted Salvage Value",
-    DiscountRate = "Model Accounting|Discount Rate",
-    InputToNewCapacity = "Technology Input|New Capacity",
-    InputToTotalCapacity = "Technology Input|Total Capacity",
-    NewCapacity = "Capacity Additions|Electricity",
-    ObjectiveValue = "Model Accounting|Objective Value",
-    ProductionByTechnologyByMode = "Secondary Energy|Electricity",
-    RateOfActivity = "Activity|Electricity",
-    RateOfProductionByTechnologyByMode = "Secondary Energy|Electricity|Rate",
-    RateOfTotalActivity = "Activity|Electricity|Total Rate",
-    RateOfUseByTechnologyByMode = "Technology Input|Rate",
-    SalvageValue = "Model Accounting|Salvage Value",
-    TechnologyEmissionsPenalty = "Cost|Emissions Penalty",
-    TotalAnnualTechnologyActivityByMode = "Activity|Electricity",
-    TotalCapacityAnnual = "Capacity|Electricity",
-    TotalTechnologyAnnualActivity = "Activity|Electricity",
-    TotalTechnologyModelPeriodActivity = "Activity|Electricity|Model Period",
-    Trade = "Trade|Energy",
-    UseByTechnologyByMode = "Technology Input"
+    AccumulatedNewCapacity =
+      "Accumulated Capacity Additions|Electricity",
+    AnnualFixedOperatingCost =
+      "Cost|Energy Supply|Electricity|Fixed O&M",
+    AnnualizedInvestmentCost =
+      "Cost|Energy Supply|Electricity|Annualized Investment",
+    AnnualTechnologyEmission =
+      "Emissions|Energy|Supply|Electricity",
+    AnnualTechnologyEmissionByMode =
+      "Emissions|Energy|Supply|Electricity",
+    AnnualVariableOperatingCost =
+      "Cost|Energy Supply|Electricity|Variable O&M",
+    CapitalInvestment =
+      "Investment|Energy Supply|Electricity",
+    Demand =
+      "Final Energy|Electricity",
+    DiscountedSalvageValue =
+      "Model Accounting|Discounted Salvage Value",
+    DiscountRate =
+      "Model Accounting|Discount Rate",
+    InputToNewCapacity =
+      "Technology Input|New Capacity",
+    InputToTotalCapacity =
+      "Technology Input|Total Capacity",
+    NewCapacity =
+      "Capacity Additions|Electricity",
+    ObjectiveValue =
+      "Model Accounting|Objective Value",
+    ProductionByTechnologyByMode =
+      "Secondary Energy|Electricity",
+    RateOfActivity =
+      "Activity|Electricity",
+    RateOfProductionByTechnologyByMode =
+      "Secondary Energy|Electricity|Rate",
+    RateOfTotalActivity =
+      "Activity|Electricity|Total Rate",
+    RateOfUseByTechnologyByMode =
+      "Technology Input|Rate",
+    SalvageValue =
+      "Model Accounting|Salvage Value",
+    TechnologyEmissionsPenalty =
+      "Cost|Emissions Penalty",
+    TotalAnnualTechnologyActivityByMode =
+      "Activity|Electricity",
+    TotalCapacityAnnual =
+      "Capacity|Electricity",
+    TotalTechnologyAnnualActivity =
+      "Activity|Electricity",
+    TotalTechnologyModelPeriodActivity =
+      "Activity|Electricity|Model Period",
+    Trade =
+      "Trade|Energy",
+    UseByTechnologyByMode =
+      "Technology Input"
   )
 
   MUS_results <- MUS %>%
     mutate(
-      e = recode(e, CO2E = "CO2"),
-      variable_iamc_base = unname(variable_mapping[variable])
+      e = recode(
+        e,
+        CO2E = "CO2"
+      ),
+      variable_iamc_base = unname(
+        variable_mapping[as.character(variable)]
+      )
     ) %>%
-    left_join(technology_map, by = "t") %>%
-    left_join(commodity_map, by = "f") %>%
+    left_join(
+      technology_map,
+      by = "t"
+    ) %>%
+    left_join(
+      commodity_map,
+      by = "f"
+    ) %>%
     mutate(
       variable_iamc = case_when(
-        variable == "AnnualEmissions" ~
-          paste("Emissions", e, sep = "|"),
-
         variable == "AnnualTechnologyEmission" ~
           paste(
-            "Emissions", e, "Energy", "Supply", "Electricity",
+            "Emissions",
+            e,
+            "Energy",
+            "Supply",
+            "Electricity",
             technology_iamc,
             sep = "|"
           ),
 
         variable == "AnnualTechnologyEmissionByMode" ~
           paste(
+            "Emissions",
+            e,
+            "Energy",
+            "Supply",
+            "Electricity",
+            technology_iamc,
+            paste0("Mode ", m),
+            sep = "|"
+          ),
+
+        variable == "Demand" ~
+          paste(
+            variable_iamc_base,
+            fuel_iamc,
+            sep = "|"
+          ),
+
+        variable == "ProductionByTechnologyByMode" ~
+          paste(
             variable_iamc_base,
             technology_iamc,
             paste0("Mode ", m),
             sep = "|"
           ),
 
-        !is.na(technology_iamc) ~
-          paste(variable_iamc_base, technology_iamc, sep = "|"),
-
-        variable == "Demand" ~
-          paste(variable_iamc_base, fuel_iamc, sep = "|"),
-
-        variable == "ProductionByTechnology" ~
+        variable == "RateOfActivity" ~
           paste(
             variable_iamc_base,
             technology_iamc,
+            paste0("Mode ", m),
             paste0("Time Slice ", l),
             sep = "|"
           ),
 
-        variable == "RateOfActivity" ~
+        variable == "RateOfProductionByTechnologyByMode" ~
           paste(
             variable_iamc_base,
+            fuel_iamc,
+            technology_iamc,
+            paste0("Mode ", m),
+            paste0("Time Slice ", l),
+            sep = "|"
+          ),
+
+        variable == "RateOfUseByTechnologyByMode" ~
+          paste(
+            variable_iamc_base,
+            fuel_iamc,
+            technology_iamc,
+            paste0("Mode ", m),
+            paste0("Time Slice ", l),
+            sep = "|"
+          ),
+
+        variable == "UseByTechnologyByMode" ~
+          paste(
+            variable_iamc_base,
+            fuel_iamc,
             technology_iamc,
             paste0("Mode ", m),
             paste0("Time Slice ", l),
@@ -318,7 +497,17 @@ calcFussionOutput <- function() {
             sep = "|"
           ),
 
-        variable == "UseByTechnologyAnnual" ~
+        variable == "Trade" ~
+          paste(
+            variable_iamc_base,
+            fuel_iamc,
+            sep = "|"
+          ),
+
+        variable %in% c(
+          "InputToNewCapacity",
+          "InputToTotalCapacity"
+        ) ~
           paste(
             variable_iamc_base,
             fuel_iamc,
@@ -326,20 +515,41 @@ calcFussionOutput <- function() {
             sep = "|"
           ),
 
-        variable == "Fuel_costs" ~
+        !is.na(technology_iamc) ~
           paste(
             variable_iamc_base,
             technology_iamc,
+            sep = "|"
+          ),
+
+        !is.na(fuel_iamc) ~
+          paste(
+            variable_iamc_base,
             fuel_iamc,
             sep = "|"
           ),
 
-        TRUE ~ variable_iamc_base
+        TRUE ~
+          variable_iamc_base
       ),
+
       variable_iamc = variable_iamc %>%
-        gsub("\\|NA(?=\\||$)", "", ., perl = TRUE) %>%
-        gsub("\\|+", "|", .) %>%
-        sub("\\|$", "", .)
+        gsub(
+          "\\|NA(?=\\||$)",
+          "",
+          .,
+          perl = TRUE
+        ) %>%
+        gsub(
+          "\\|+",
+          "|",
+          .
+        ) %>%
+        sub(
+          "\\|$",
+          "",
+          .
+        )
     )
 
   for (df in c("DRC_results", "ETH_results", "MUS_results")) {
